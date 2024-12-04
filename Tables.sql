b540e6511e91bf712036c911215fd6fd28002391
sarena <sareena.aul@student.mahidol.edu>
2024-12-04 16:16:00 +0700

    Create Tables (new ver)

--- Tables.sql
+++ Tables.sql
@@ -1,30 +1,34 @@
--- Active: 1733124735108@@dpg-ct6kb61u0jms7399gihg-a.singapore-postgres.render.com@5432
-CREATE TABLE User (
-    User_ID INT PRIMARY KEY AUTO_INCREMENT,
+-- Active: 1733124735108@@dpg-ct6kb61u0jms7399gihg-a.singapore-postgres.render.com@5432@my_database@public
+
+CREATE TABLE Users (
+    User_ID INT PRIMARY KEY,
     Name VARCHAR(100),
     Email VARCHAR(100) UNIQUE,
     Password VARCHAR(100),
     Phone_Number VARCHAR(15)
 );
-
+        
+DROP TABLE products;
 CREATE TABLE Products (
-    Product_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Product_ID INT PRIMARY KEY,
+    Product_Name VARCHAR(255),
     Price DECIMAL(10, 2)
 );
 
 CREATE TABLE Orders (
-    Order_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Order_ID INT PRIMARY KEY,
     User_ID INT,
     Order_Date DATE,
     Total_Price DECIMAL(10, 2),
     Status VARCHAR(50),
-    FOREIGN KEY (User_ID) REFERENCES User(User_ID)
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
 );
 
 CREATE TABLE Items (
-    Order_Item_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Order_Item_ID INT PRIMARY KEY,
     Order_ID INT,
     Product_ID INT,
+    Product_Name VARCHAR(255),
     Quantity INT,
     Price DECIMAL(10, 2),
     FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID),
@@ -36,5 +40,3 @@ CREATE TABLE Inventory (
     Stock_Quantity INT,
     FOREIGN KEY (Product_ID) REFERENCES Products(Product_ID)
 );
-
---just creating tables here 
