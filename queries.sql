197c8962af224f30716ccba41acc3a7c414f7a3c
sarena <sareena.aul@student.mahidol.edu>
2024-12-05 04:48:40 +0700

    updated check stock

--- queries.sql
+++ queries.sql
@@ -12,19 +12,15 @@ FROM Orders o
 JOIN User u ON o.User_ID = u.User_ID;
 
 SELECT * 
-FROM Orders
-LIMIT 1;
+FROM Inventory i
+JOIN Items t ON i.Product_ID = t.Product_ID
+WHERE t.Order_ID = 1;
+
+
 
 UPDATE Inventory
-SET Stock_Quantity = Stock_Quantity - o.Ordered_Quantity
-FROM Orders o
-WHERE Inventory.Product_ID = o.product_id AND Inventory.Stock_Quantity > 0;
-
-SELECT Product_Name, Stock_Quantity
-FROM Inventory
-JOIN Products ON Inventory.Product_ID = Products.Product_ID
-WHERE Stock_Quantity < 10;
-
-SELECT SUM(Products.Price * Inventory.Stock_Quantity) AS Total_Inventory_Value
-FROM Products
-JOIN Inventory ON Products.Product_ID = Inventory.Product_ID;
+SET Stock_Quantity = Inventory.Stock_Quantity - i.Quantity
+FROM Items i
+WHERE Inventory.Product_ID = i.Product_ID
+  AND i.Order_ID = 1
+  AND Inventory.Stock_Quantity >= i.Quantity;
