438f4d522f7aa0ad0d849a0bb96d9ad7b70e5e67
sarena <sareena.aul@student.mahidol.edu>
2024-12-05 04:53:13 +0700

    last update

--- queries.sql
+++ queries.sql
@@ -11,16 +11,22 @@ SELECT o.Order_ID, o.Order_Date, o.Total_Price, u.Name, u.Email
 FROM Orders o
 JOIN User u ON o.User_ID = u.User_ID;
 
-SELECT * 
-FROM Inventory i
-JOIN Items t ON i.Product_ID = t.Product_ID
-WHERE t.Order_ID = 1;
-
-
-
 UPDATE Inventory
 SET Stock_Quantity = Inventory.Stock_Quantity - i.Quantity
 FROM Items i
 WHERE Inventory.Product_ID = i.Product_ID
-  AND i.Order_ID = 1
   AND Inventory.Stock_Quantity >= i.Quantity;
+
+
+
+WITH TotalQuantities AS (
+    SELECT Product_ID, SUM(Quantity) AS Total_Quantity
+    FROM Items
+    GROUP BY Product_ID
+)
+UPDATE Inventory
+SET Stock_Quantity = Inventory.Stock_Quantity - t.Total_Quantity
+FROM TotalQuantities t
+WHERE Inventory.Product_ID = t.Product_ID
+  AND Inventory.Stock_Quantity >= t.Total_Quantity;
+
