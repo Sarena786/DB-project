483a426bbab8fb3fe6d6f05574060a9b09405370
81141 <46625054+81141@users.noreply.github.com>
2024-12-04 19:31:25 +0700

    fix: project.sql

--- project.sql
+++ project.sql
@@ -1,6 +1,7 @@
-SOURCE create_tables.sql;
+SOURCE Table_A.sql;
+SOURCE Tables.sql;
 SOURCE insert.sql;
 SOURCE queries.sql;
 SOURCE functions_procedures.sql;
-
+SOURCE productandInventory.sql;
 --to run everything in one go
