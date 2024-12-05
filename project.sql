9b1f611f13b97d5a0404b702993b6ee4f6a0640f
81141 <46625054+81141@users.noreply.github.com>
2024-12-05 14:33:23 +0700

    commenting out
    comment out inventory

--- project.sql
+++ project.sql
@@ -3,5 +3,5 @@ SOURCE Tables.sql;
 SOURCE insert.sql;
 SOURCE queries.sql;
 SOURCE functions_procedures.sql;
-SOURCE productandInventory.sql;
+-- SOURCE productandInventory.sql;
 --to run everything in one go
